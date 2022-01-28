5e4b4c83a5d581d7d35fde54d945d4e07d21322f
Getahun Assefa <getitpro@gmail.com>
2022-01-28 15:53:29 +0100

    Add project files.

---
+++ Garage_2.0/Models/Entities/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2._0.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public bool Parked { get; set; }
+        [Required]
+        [StringLength(6)]
+        public string RegNo { get; set; }
+        
+        public DateTime ArrivalTime { get; set; }
+        [StringLength(20)]
+        public string Brand { get; set; }
+        [StringLength(15)]
+        public string Model { get; set; }
+        [Range(2, 16)]
+        public int NoOfWheels { get; set; }
+        [StringLength(15)]
+        public string Color { get; set; }
+        public Vehicle vehicleType { get; set; }
+    }
+}
---
+++ Garage_2.0/Models/Entities/VehicleType.cs
@@ -0,0 +1,12 @@
+﻿namespace Garage_2._0.Models
+{
+    public enum VehicleType
+    {
+        NotAvailable,
+        Car,
+        Van,
+        Lorry,
+        Bus,
+        MotorCycle
+    }
+}

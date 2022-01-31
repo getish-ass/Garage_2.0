8d3ea4b129b6d50115e7a667405ea2156e88bcf9
Getahun Assefa <getitpro@gmail.com>
2022-01-31 13:39:53 +0100

    lite correction on Model

--- Garage_2.0/Models/Entities/Vehicle.cs
+++ Garage_2.0/Models/Entities/Vehicle.cs
@@ -11,14 +11,15 @@ namespace Garage_2._0.Models
         public string RegNo { get; set; }
         
         public DateTime ArrivalTime { get; set; }
-        [StringLength(20)]
+        [StringLength(25)]
         public string Brand { get; set; }
-        [StringLength(15)]
+        [StringLength(20)]
         public string Model { get; set; }
         [Range(2, 16)]
         public int NoOfWheels { get; set; }
-        [StringLength(15)]
+        [StringLength(20)]
         public string Color { get; set; }
+
         public Vehicle vehicleType { get; set; }
     }
 }

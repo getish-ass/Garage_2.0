29959e8102cf0a011ad6c426368e2cb98be2229a
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2022-02-02 15:13:59 +0100

    Edit Vehicle and Viewmodel so that output is correct.

--- Garage_2.0/Models/Entities/Vehicle.cs
+++ Garage_2.0/Models/Entities/Vehicle.cs
@@ -5,6 +5,7 @@ namespace Garage_2._0.Models
     public class Vehicle
     {
         public int Id { get; set; }
+        
         [Display(Name = "Parkerad")]
         public bool Parked { get; set; }
 
@@ -12,20 +13,26 @@ namespace Garage_2._0.Models
         [StringLength(6)]
         [Display(Name = "Reg-nr")]
         public string? RegNo { get; set; }
+        
         [Display(Name = "Ankomsttid")]
         public DateTime ArrivalTime { get; set; }
+        
         [StringLength(25)]
         [Display(Name = "Märke")]
         public string? Brand { get; set; }
+
         [StringLength(20)]
         [Display(Name = "Modell")]
         public string? Model { get; set; }
+        
         [Range(2, 16)]
         [Display(Name = "Antal hjul")]
         public int NoOfWheels { get; set; }
+        
         [StringLength(20)]
         [Display(Name ="Färg")]
         public string? Color { get; set; }
+        
         [Display(Name = "Fordonstyp")]
         public VehicleType VehicleType { get; set; }
     }
--- Garage_2.0/Models/ViewModels/VehicleIndexViewModel.cs
+++ Garage_2.0/Models/ViewModels/VehicleIndexViewModel.cs
@@ -8,12 +8,14 @@ namespace Garage_2._0.Models.ViewModels
     {
         public int Id { get; set; }
         [Display(Name = "Parkerad")]
+
         public bool Parked { get; set; }
 
         [Required]
         [StringLength(6)]
         [Display(Name = "Reg-nr")]
         public string? RegNo { get; set; }
+
         [Display(Name = "Ankomsttid")]
         public DateTime ArrivalTime { get; set; }
   

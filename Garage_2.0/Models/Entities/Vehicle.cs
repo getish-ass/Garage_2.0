7f405fd5927ad731d516c4ed979afbd1abfc55e8
Artin <alex_91_100@live.se>
2022-02-03 01:37:03 +0100

    Some changes

--- Garage_2.0/Models/Entities/Vehicle.cs
+++ Garage_2.0/Models/Entities/Vehicle.cs
@@ -9,7 +9,6 @@ namespace Garage_2._0.Models
         [Required]
         [StringLength(6)]
         public string? RegNo { get; set; }
-        
         public DateTime ArrivalTime { get; set; }
         [StringLength(25)]
         public string? Brand { get; set; }
@@ -19,7 +18,6 @@ namespace Garage_2._0.Models
         public int NoOfWheels { get; set; }
         [StringLength(20)]
         public string? Color { get; set; }
-
         public VehicleType vehicleType { get; set; }
     }
 }

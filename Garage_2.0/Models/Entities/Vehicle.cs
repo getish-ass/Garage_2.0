03c66ada0ac67c033dfa52e877a9df0c08cb42bc
Artin <alex_91_100@live.se>
2022-02-03 01:53:46 +0100

    Fler omvandlingar till svenska

--- Garage_2.0/Models/Entities/Vehicle.cs
+++ Garage_2.0/Models/Entities/Vehicle.cs
@@ -5,18 +5,32 @@ namespace Garage_2._0.Models
     public class Vehicle
     {
         public int Id { get; set; }
+
+        [Display(Name = "Parkerad")]
         public bool Parked { get; set; }
+
         [Required]
         [StringLength(6)]
+        [Display(Name = "Reg-nr")]
         public string? RegNo { get; set; }
+
+        [Display(Name = "Ankomsttid")]
         public DateTime ArrivalTime { get; set; }
         [StringLength(25)]
+
+        [Display(Name = "Märke")]
         public string? Brand { get; set; }
+
         [StringLength(20)]
+        [Display(Name = "Modell")]
         public string? Model { get; set; }
+
         [Range(2, 16)]
+        [Display(Name = "Antal hjul")]
         public int NoOfWheels { get; set; }
+
         [StringLength(20)]
+        [Display(Name = "Färg")]
         public string? Color { get; set; }
         public VehicleType vehicleType { get; set; }
     }
